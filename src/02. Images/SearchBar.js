9a57cabe67c4a9823097b37508a744b00a3fcc1a
codeWithAshith <codewithashith@gmail.com>
2023-05-01 16:31:39 +0530

    17. Adding controlled input

--- src/02. Images/SearchBar.js
+++ src/02. Images/SearchBar.js
@@ -1,16 +1,29 @@
 import { Component } from "react";
 
 class SearchBar extends Component {
+  state = { term: "" };
+
+  handleInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
   render() {
+    // const { term } = this.state;
+
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" placeholder="Search" />
+            <input
+              type="text"
+              placeholder="Search"
+              value={this.state.term}
+              onChange={this.handleInputChange}
+            />
           </div>
         </form>
-      </div> 
+      </div>
     );
   }
 }
