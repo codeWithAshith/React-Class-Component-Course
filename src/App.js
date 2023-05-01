5870ce3bf278004f65af9a54d49eda61707ca7a6
codeWithAshith <codewithashith@gmail.com>
2023-05-01 13:32:43 +0530

    02. Setting State

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, lon: null };
+  }
+
   render() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        });
+      },
       (err) => console.log(err)
     );
 
     return (
       <div>
-        <h1> Hi there</h1>
+        <h1>Lat: {this.state.lat}</h1>
+        <h1>Lon: {this.state.lon}</h1>
       </div>
     );
   }
