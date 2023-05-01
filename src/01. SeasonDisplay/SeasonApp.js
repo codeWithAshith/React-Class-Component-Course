ba7878a3753e4cb07afc90012974558883a80a26
codeWithAshith <codewithashith@gmail.com>
2023-05-01 16:13:31 +0530

    15. Restructing folders

---
+++ src/01. SeasonDisplay/SeasonApp.js
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "../Spinner";
+
+class SeasonApp extends Component {
+  state = { lat: null, errorMessage: null };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        if (!this.state.lat)
+          this.setState({
+            lat: position.coords.latitude,
+          });
+      },
+      (err) =>
+        this.setState({
+          errorMessage: err.message,
+        })
+    );
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} month={new Date().getMonth} />;
+    }
+
+    return <Spinner message="Please accept the location request" />;
+  }
+  render() {
+    return <div>{this.renderContent()}</div>;
+  }
+}
+
+export default SeasonApp;
--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
 import { Component } from "react";
-import SeasonDisplay from "./SeasonDisplay";
-import Spinner from "./Spinner";
-
+import SeasonApp from "./01. SeasonDisplay/SeasonApp";
 class App extends Component {
-  state = { lat: null, errorMessage: null };
 
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        if (!this.state.lat)
-          this.setState({
-            lat: position.coords.latitude,
-          });
-      },
-      (err) =>
-        this.setState({
-          errorMessage: err.message,
-        })
-    );
-  }
-
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} month={new Date().getMonth} />;
-    }
-
-    return <Spinner message="Please accept the location request" />;
-  }
 
   render() {
-    return <div>{this.renderContent()}</div>;
+    return <SeasonApp></SeasonApp>;
   }
 }
 
