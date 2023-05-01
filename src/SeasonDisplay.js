2cb332300211e6b601fe08c30df330160557209a
codeWithAshith <codewithashith@gmail.com>
2023-05-01 15:44:13 +0530

    10. Adding Season config

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,24 +1,34 @@
 import { Component } from "react";
 
 class SeasonDisplay extends Component {
+  seasonConfig = {
+    summer: {
+      text: "Lets hit the beach",
+      iconName: "sun",
+    },
+    winter: {
+      text: "Burr, it is chilly",
+      iconName: "snowflake",
+    },
+  };
+
   getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
-      return lat > 0 ? "Summer" : "Winter";
+      return lat > 0 ? "summer" : "winter";
     } else {
-      return lat > 0 ? "Winter" : "Summer";
+      return lat > 0 ? "winter" : "summer";
     }
   };
+  
   render() {
     const season = this.getSeason(this.props.lat, this.props.month);
-    const text =
-      season === "Summer" ? "Lets hit the beach" : "Burr, it is chilly";
-    const icon = season === "Summer" ? "sun" : "snowflake";
+    const { text, iconName } = this.seasonConfig[season];
 
     return (
       <div>
-        <i className={`${icon} icon`} />
+        <i className={`${iconName} icon`} />
         <h2>{text}</h2>
-        <i className={`${icon} icon`} />
+        <i className={`${iconName} icon`} />
       </div>
     );
   }
