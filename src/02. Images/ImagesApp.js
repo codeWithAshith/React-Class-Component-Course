45dcf0a52a9261ce522c01c61ff0a341145ff30f
codeWithAshith <codewithashith@gmail.com>
2023-05-02 13:39:22 +0530

    19. Fetching images from api

--- src/02. Images/ImagesApp.js
+++ src/02. Images/ImagesApp.js
@@ -1,6 +1,10 @@
 import { Component } from "react";
 import SearchBar from "./SearchBar";
 
+// https://unsplash.com/developers
+const baseUrl = "https://api.unsplash.com/search/photos";
+const clientId = "7_Ep1kcx5FU-RXd0nRkyujZMEn0sYEhZhPhFn8Ck1LU";
+
 class ImagesApp extends Component {
   state = { term: "" };
 
@@ -8,10 +12,21 @@ class ImagesApp extends Component {
     this.setState({ term: event.target.value });
   };
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(this.state.term);
+
+    try {
+      const response = await fetch(
+        `${baseUrl}?client_id=${clientId}&page=1&query=${this.state.term}`
+      );
+      const images = await response.images();
+
+      console.log(JSON.parse(images));
+    } catch (err) {
+      console.log(err);
+    }
   };
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
